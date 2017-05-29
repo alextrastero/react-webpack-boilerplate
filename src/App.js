8a0007adc34ccf5f4552379e3545550fd088fbe3
Alexander Odell <alextrastero@gmail.com>
2017-05-29 17:07:50 +0200

    pwpw

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import Autocomplete from './react-materialize/Autocomplete';
+import Slider from './react-materialize/Slider';
+import Slide from './react-materialize/Slide';
 
 const App = () => {
   const data = {
@@ -8,7 +9,25 @@ const App = () => {
   }
   return (
     <div>
-      <Autocomplete data={data} />
+      <Slider>
+        <Slide
+          src="http://lorempixel.com/580/250/nature/1"
+          title="This is our big Tagline!">
+          Here's our small slogan.
+        </Slide>
+        <Slide
+          src="http://lorempixel.com/580/250/nature/2"
+          title="Left aligned Caption"
+          placement="left">
+          Here's our small slogan.
+        </Slide>
+        <Slide
+          src="http://lorempixel.com/580/250/nature/3"
+          title="Right aligned Caption"
+          placement="right">
+          Here's our small slogan.
+        </Slide>
+      </Slider>
     </div>
   )
 };
