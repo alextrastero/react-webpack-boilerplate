f7c3c6dd00e0a7c27fc7e7fc0b2f76b0a33d03a1
Alexander Odell <alextrastero@gmail.com>
2017-05-29 17:26:48 +0200

    a

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from 'react';
-import Slider from './react-materialize/Slider';
-import Slide from './react-materialize/Slide';
+import Tag from './react-materialize/Tag';
+import Icon from './react-materialize/Icon';
 
 const App = () => {
-  const data = {
-    one: null,
-    two: null
-  }
   return (
     <div>
-      <Slider>
-        <Slide
-          src="http://lorempixel.com/580/250/nature/1"
-          title="This is our big Tagline!">
-          Here's our small slogan.
-        </Slide>
-        <Slide
-          src="http://lorempixel.com/580/250/nature/2"
-          title="Left aligned Caption"
-          placement="left">
-          Here's our small slogan.
-        </Slide>
-        <Slide
-          src="http://lorempixel.com/580/250/nature/3"
-          title="Right aligned Caption"
-          placement="right">
-          Here's our small slogan.
-        </Slide>
-      </Slider>
+      <Tag>pepe</Tag>
     </div>
   )
 };
