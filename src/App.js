65b2f6e9d7985874d6f31411ac62d0b141e0d482
Alexander Odell <alextrastero@gmail.com>
2017-06-21 17:02:34 +0200

    pepe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import SideNav from './react-materialize/SideNav';
+import { Input } from './react-materialize';
 
-const App = () => {
-  return (
-    <SideNav trigger={<span>click me</span>}/>
-  )
-};
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      label: 'INITIAL LABEL'
+    }
+  }
+
+  componentDidMount() {
+    $('.datepicker').pickadate({
+      selectMonths: true, // Creates a dropdown to control month
+      selectYears: 15, // Creates a dropdown of 15 years to control year
+      onSet: this.handleDateChange // returns { select: dateInMilliseconds }
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        <Input ref='date' type='date' />
+      </div>
+    );
+  }
+}
 
 export default App;
