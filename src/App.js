147240d1e8d26f226dd18dda11c28a1300a63ff3
Alexander Odell <alextrastero@gmail.com>
2017-09-12 10:15:34 +0200

    YUOL

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from 'react';
-import { Navbar, Col, Row, NavItem } from './react-materialize';
+import { Modal, Button } from './react-materialize';
 
 class App extends React.Component {
   render () {
     return (
-      <Row className='red lighten-2'>
-        <Col s={12}>
-          <Navbar>
-            <NavItem key='foo'>
-              Home
-            </NavItem>
-          </Navbar>
-        </Col>
-      </Row>
+      <Modal trigger={<Button>trigger</Button>}>
+        <p>here we are</p>
+      </Modal>
     );
   }
 }
