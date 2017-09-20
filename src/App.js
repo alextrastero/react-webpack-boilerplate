1ea5e83273500d5c58186798a571d10b5b2d5461
Alexander Odell <alextrastero@gmail.com>
2017-09-20 14:32:35 +0200

    ll

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import { Modal, Button } from './react-materialize';
+import { Card, CardTitle, Col, Row } from './react-materialize';
 
 class App extends React.Component {
   render () {
     return (
-      <Modal trigger={<Button>trigger</Button>}>
-        <p>here we are</p>
-      </Modal>
+      <Row>
+        <Col s={4}>
+          <Card header={<CardTitle reveal image={'https://placeimg.com/100/100/any'} waves='light' />}
+            title='Card Title'
+            reveal={<p>Here is some more information about this product that is only revealed once clicked on.</p>}>
+            <p><a href='#'>This is a link</a></p>
+          </Card>
+        </Col>
+      </Row>
     );
   }
 }
