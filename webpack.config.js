ac565b0ff9632ad902d741e0e27d2e48eee7a581
Alexander Odell <alextrastero@gmail.com>
2017-09-24 12:56:01 +0200

    Fix configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,67 +1,74 @@
 const { resolve } = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge')
 
 const PORT = 3000;
 
-module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    // activate HMR for React
+const targetEnv = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development'
 
-    `webpack-dev-server/client?http://localhost:${PORT}`,
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
-
-    'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
-
-    './index.js'
-    // the entry point of our app
-  ],
+const config = {
   output: {
     filename: 'bundle.js',
-    // the output bundle
-
     path: resolve(__dirname, 'dist'),
-
     publicPath: '/'
-    // necessary for HMR to know where to load the hot update chunks
   },
-
   context: resolve(__dirname, 'src'),
-
-  devtool: 'inline-source-map',
-
-  devServer: {
-    hot: true,
-    // enable HMR on the server
-
-    contentBase: resolve(__dirname, 'dist'),
-    // match the output path
-
-    publicPath: '/',
-    // match the output `publicPath`
-
-    port: PORT
-    // dev server PORT
-  },
-
   module: {
     rules: [{
       test: /\.js$/,
-      use: [
-        'babel-loader'
-      ],
-      exclude: /node_modules/
+      exclude: /node_modules/,
+      use: 'babel-loader'
     }]
   },
-
   plugins: [
+    new HtmlWebpackPlugin({
+      template: '../dist/index.html'
+    }),
     // enable HMR globally
     new webpack.HotModuleReplacementPlugin(),
 
     // prints more readable module names in the browser console on HMR updates
     new webpack.NamedModulesPlugin()
   ]
+}
+
+if (targetEnv === 'development') {
+  module.exports = merge(config, {
+    entry: [
+      'react-hot-loader/patch',
+      // activate HMR for React
+
+      `webpack-dev-server/client?http://localhost:${PORT}`,
+      // bundle the client for webpack-dev-server
+      // and connect to the provided endpoint
+
+      'webpack/hot/only-dev-server',
+      // bundle the client for hot reloading
+      // only- means to only hot reload for successful updates
+
+      './index.js'
+      // the entry point of our app
+    ],
+    devServer: {
+      hot: true,
+      contentBase: resolve(__dirname, 'dist'),
+      publicPath: '/',
+      port: PORT
+    },
+    devtool: 'inline-source-map',
+  })
+}
+
+if (targetEnv === 'production') {
+  module.exports = merge(config, {
+    entry: './index.js',
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      })
+    ]
+  })
 }
