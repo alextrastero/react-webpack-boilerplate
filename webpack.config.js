b3b07dce788e2ddf1aa504ef1521f6dc5764d219
Alexander Odell <alextrastero@gmail.com>
2017-09-30 11:30:05 +0200

    poop

--- webpack.config.js
+++ webpack.config.js
@@ -1,89 +1,85 @@
 const { resolve } = require('path')
 const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge')
 
 const PORT = 3000;
 
-module.exports = {
-  entry: [
-    'react-hot-loader/patch',
-    // activate HMR for React
+const targetEnv = process.env.npm_lifecycle_event === 'build' ? 'production' : 'development'
 
-    `webpack-dev-server/client?http://localhost:${PORT}`,
-    // bundle the client for webpack-dev-server
-    // and connect to the provided endpoint
-
-    'webpack/hot/only-dev-server',
-    // bundle the client for hot reloading
-    // only- means to only hot reload for successful updates
-
-    './index.js'
-    // the entry point of our app
-  ],
+const config = {
+  entry: './index.js',
   output: {
     filename: 'bundle.js',
-    // the output bundle
-
     path: resolve(__dirname, 'dist'),
-
     sourceMapFilename: 'bundle.js.map',
-
     publicPath: '/'
-    // necessary for HMR to know where to load the hot update chunks
   },
-
   context: resolve(__dirname, 'src'),
-
-  devtool: 'eval',
-
-  devServer: {
-    hot: true,
-    // enable HMR on the server
-
-    contentBase: resolve(__dirname, 'dist'),
-    // match the output path
-
-    publicPath: '/',
-    // match the output `publicPath`
-
-    port: PORT
-    // dev server PORT
-  },
-
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: [
-          'babel-loader'
-        ],
-        exclude: /node_modules/
+        exclude: /node_modules/,
+        use: 'babel-loader'
       },
       {
-        test: /\.css$/,
+        test: /\.scss$/,
         use: [
-          'style-loader',
-          'css-loader?modules&importLoaders=1',
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: function () {
-                return [
-                  require('postcss-cssnext'),
-                  require('postcss-nested')
-                ]
-              }
-            }
-          }
+          { loader: 'style-loader' },
+          { loader: 'css-loader?sourceMap?convertToAbsoluteUrls' },
+          { loader: 'sass-loader?sourceMap' }
         ]
       }
     ]
   },
-
   plugins: [
+    new HtmlWebpackPlugin({
+      template: '../dist/index.html'
+    }),
     // enable HMR globally
     new webpack.HotModuleReplacementPlugin(),
 
     // prints more readable module names in the browser console on HMR updates
     new webpack.NamedModulesPlugin()
   ]
+}
+
+if (targetEnv === 'development') {
+  module.exports = merge(config, {
+    entry: [
+      'react-hot-loader/patch',
+      // activate HMR for React
+
+      `webpack-dev-server/client?http://localhost:${PORT}`,
+      // bundle the client for webpack-dev-server
+      // and connect to the provided endpoint
+
+      'webpack/hot/only-dev-server',
+      // bundle the client for hot reloading
+      // only- means to only hot reload for successful updates
+
+      './index.js'
+      // the entry point of our app
+    ],
+    devServer: {
+      hot: true,
+      contentBase: resolve(__dirname, 'dist'),
+      publicPath: '/',
+      port: PORT
+    },
+    devtool: 'eval-source-map',
+  })
+}
+
+if (targetEnv === 'production') {
+  module.exports = merge(config, {
+    plugins: [
+      new webpack.DefinePlugin({
+        'process.env': {
+          NODE_ENV: JSON.stringify('production')
+        }
+      })
+    ]
+  })
 }
